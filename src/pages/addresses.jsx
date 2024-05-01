679cffc7df9359794d652eb04b5a2db07be9e30e
allanthieryd <allan.td@free.fr>
2024-05-01 22:56:52 +0200

    Modification on the filter

--- src/pages/addresses.jsx
+++ src/pages/addresses.jsx
@@ -1,7 +1,6 @@
-import { useState, useRef, useEffect } from "react"
+import { useState } from "react"
 import "@fontsource/montserrat"
 import axios from "axios"
-import Filter from "@/components/index/Filter"
 import AddressTable from "@/components/index/AddressTable"
 
 export const getServerSideProps = async () => {
@@ -16,39 +15,9 @@ export const getServerSideProps = async () => {
 const AddressPage = (props) => {
   const { addresses: initialAddresses } = props
   const [addresses, setAddresses] = useState(initialAddresses)
-  const [isMenuOpen, setMenuOpen] = useState(false)
-  const menuRef = useRef(null)
-  const [distance, setDistance] = useState(50)
-  const handleToggleMenu = (event) => {
-    event.stopPropagation()
-    setMenuOpen(!isMenuOpen)
-  }
-  const handleCloseMenu = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      setMenuOpen(false)
-    }
-  }
-  const handleDistanceChange = (newDistance) => {
-    setDistance(newDistance)
-  }
-
-  useEffect(() => {
-    document.addEventListener("click", handleCloseMenu)
-
-    return () => {
-      document.removeEventListener("click", handleCloseMenu)
-    }
-  }, [])
 
   return (
     <main className="dark:bg-gray-900 dark:text-white">
-      <Filter
-        isMenuOpen={isMenuOpen}
-        handleToggleMenu={handleToggleMenu}
-        menuRef={menuRef}
-        distance={distance}
-        handleDistanceChange={handleDistanceChange}
-      />
       <AddressTable addresses={addresses} setAddresses={setAddresses} />
     </main>
   )
